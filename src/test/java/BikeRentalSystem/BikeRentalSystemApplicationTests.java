4992dc03fafb48dd6be39836643d63b8c955eff5
mikalikahi <sweeney.phil@gmail.com>
2023-01-18 10:54:21 +0800

    First commit of basic project structure files

---
+++ src/test/java/BikeRentalSystem/BikeRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package BikeRentalSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

71ecaaa3651e977b8c46c94bf927eacb7695cd46
mikalikahi <sweeney.phil@gmail.com>
2023-01-19 11:15:41 +0800

    Authentication logic to display different links to different user types in header and different content on landing page. Beginning of controller logic to submit form for users (customers) to edit their user meta (profiles)

--- src/main/java/BikeRentalSystem/controllers/HomeController.java
+++ src/main/java/BikeRentalSystem/controllers/HomeController.java
@@ -1,18 +1,28 @@
 package BikeRentalSystem.controllers;
+import BikeRentalSystem.models.UserMeta;
+import BikeRentalSystem.models.UserPrincipal;
+import BikeRentalSystem.services.CustomUserService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @Controller
 public class HomeController {
+    @Autowired
+    CustomUserService userDetailService;
     @GetMapping("/")
-    public String homePage() {
+    public String homePage(Authentication authentication, Model model) {
+        UserPrincipal user = (UserPrincipal) authentication.getPrincipal();
+        System.out.println("This is the user: " + user);
+        model.addAttribute("user", user);
         return "authentication/home";
     }
 
@@ -22,7 +32,19 @@ public class HomeController {
    // }
 
     @GetMapping("/customer")
-    public String superUPage() {
-        return "authentication/customer";
+    public ModelAndView showUserMetaEditPage(Authentication authentication) {
+        UserPrincipal user = (UserPrincipal) authentication.getPrincipal();
+        ModelAndView mav = new ModelAndView("edit-user-meta");
+        UserMeta userMeta = user.getUserMeta();
+        if (userMeta == null) {
+            mav.setViewName("error");
+            mav.addObject("message",
+                    "Customer with id "
+                            + user.getId() + " does not exist."
+            );
+        } else {
+            mav.addObject("customer", userMeta);
+        }
+        return mav;//"authentication/customer";
     }
 }
